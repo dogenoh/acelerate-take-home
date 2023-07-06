bb49458c4f2afb6c0984ce2f7b4e1215724f56d7
Kun Ok Noh <bnohcub@hotmail.com>
2023-07-05 20:11:13 -0700

    made code modular, created components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import RestaurantForm from './components/RestaurantForm';
+import NewReviews from './components/NewReviews';
+import Reviews from './components/Reviews';
 
 type Restaurant = {
   id: number;
@@ -67,22 +70,11 @@ function App() {
 
   return (
     <div className="h-screen w-screen flex flex-col">
-      <div className="self-center">
-        <h1 className="text-3xl font-bold mb-4">Acelerate Take-Home</h1>
-        <input
-          type="text"
-          value={restaurant}
-          placeholder="Search restaurants"
-          onChange={(e) => setRestaurant(e.target.value)}
-          className="px-4 py-2 border border-gray-300 rounded-md mb-4"
-        />
-        <button
-          onClick={scrapeData}
-          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-        >
-          Get Ratings &amp; Reviews
-        </button>
-      </div>
+      <RestaurantForm
+        restaurant={restaurant}
+        setRestaurant={setRestaurant}
+        onScrapeData={scrapeData}
+      />
       {data && (
         <div>
           <h2 className="text-2xl font-bold mt-6 mb-4">
@@ -95,45 +87,12 @@ function App() {
             Refresh Data
           </button>
           <h3 className="text-xl font-bold mt-6 mb-2">Reviews</h3>
-          {newReviews && newReviews.length > 0 ? (
-            <div className="mt-6">
-              <h3 className="text-xl font-bold mb-2">New Reviews</h3>
-              <div className="grid gap-4">
-                {newReviews.map((review) => (
-                  <div
-                    key={review.id}
-                    className="bg-green-100 rounded-md shadow-md p-4"
-                  >
-                    <p className="text-lg font-bold mb-2">
-                      Rating: {review.rating}/5
-                    </p>
-                    <p className="text-gray-500 mb-2">
-                      Reviewer: {review.source}
-                    </p>
-                    <p>{review.text}</p>
-                    <p className="text-gray-500 mt-2">Date: {review.date}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
+          {newReviews.length ? (
+            <NewReviews newReviews={newReviews} />
           ) : newReviews === null ? null : (
             <div className="mt-6 text-gray-500">No New Reviews</div>
           )}
-          <div className="grid gap-4">
-            {data.reviews.map((review) => (
-              <div
-                key={review.id}
-                className="bg-white rounded-md shadow-md p-4"
-              >
-                <p className="text-lg font-bold mb-2">
-                  Rating: {review.rating}/5
-                </p>
-                <p className="text-gray-500 mb-2">Reviewer: {review.source}</p>
-                <p>{review.text}</p>
-                <p className="text-gray-500 mt-2">Date: {review.date}</p>
-              </div>
-            ))}
-          </div>
+          <Reviews reviews={data.reviews} />
         </div>
       )}
     </div>
